55c6e8e30e373b55bc4628b2bb998d0bc99f2de9
fabriciorangel07 <fabricio3176@gmail.com>
2021-09-16 07:11:19 -0300

    Aulas do neto

--- 06_merge_sort.py
+++ 06_merge_sort.py
@@ -5,11 +5,20 @@
 #   Em seguida, usando a técnica de mesclagem (merge), "remonta" o vetor,
 #   dessa vez com os elementos já em ordem.
 
+comps = 0
+divisoes = 0
+juncoes = 0
+
 def merge_sort(lista):
     """
         Função que implementa o algoritmo merge sort usando o
         método RECURSIVO
     """
+
+    # Não podemos zerar as variáveis globais de estatística
+    # dentro da função porque ela é recursiva e resetaria
+    # a contagem a cada chamada
+    global comps, divisoes, juncoes
 
     # print(f'Lista recebida: {lista}')
 
@@ -22,7 +31,9 @@ def merge_sort(lista):
     # Gera cópia da primeira metade da lista
     lista_esq = lista[:meio]    # Do início ao meio - 1
     # Gera cópia da segunda metade da lista
-    lista_dir = lista[meio:]    # Do meio ao fim
+    lista_dir = lista[meio:]    # Do meio ao 
+    
+    divisoes += 1
 
     # Chamamos recursivamente a função para continuar
     # repartindo a lista em metades
@@ -49,6 +60,7 @@ def merge_sort(lista):
         else:
             ordenada.append(lista_dir[pos_dir])
             pos_dir += 1
+        comps += 1
 
     sobra = None    # A sobra da lista que ficou para trás
 
@@ -59,10 +71,15 @@ def merge_sort(lista):
 
     # print(f'>>>> final ordenada: {ordenada + sobra}')
 
-    # Retornamos a lista final ordenada, composta da ordenada + sobra
+    # Retornamos a lista final ordenada, composta da ordenada + 
+    juncoes += 1
     return ordenada + sobra     # "Soma" de duas listas
 
 #################################################################
+
+comps = 0
+divisoes = 0
+juncoes = 0
 
 nums = [88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
 
@@ -75,18 +92,25 @@ print(nums_ord)
 from data.nomes_desord import nomes
 from time import time
 import tracemalloc
+import psutil
+
+comps = 0
+divisoes = 0
+juncoes = 0
 
 ini = time()
-tracemalloc.start()     # Inicia a medição de consumo de memória
+#tracemalloc.start()     # Inicia a medição de consumo de memória
 
 nomes_ord = merge_sort(nomes)
 
-mem_atual, mem_pico = tracemalloc.get_traced_memory()
+#mem_atual, mem_pico = tracemalloc.get_traced_memory()
 
 fim = time()
 
-print(nomes)
+print(nomes_ord)
 print(f"Tempo: {fim - ini}")
-print(f"Passadas: {passadas}, comparações: {comps}, trocas: {trocas}",""Pico de memória: {mem_pico / 1024 / 1024}MB")
+# print(f"Pico de memória: {mem_pico / 1024 / 1024}MB")
+print(f"Comparações: {comps}, divisões: {divisoes}, junções: {juncoes}")
 
-tracemalloc.stop()      # Finaliza a medição do consumo de memória
+# tracemalloc.stop()      # Finaliza a medição do consumo de memória
+print('The CPU usage is: ', psutil.cpu_percent(4))
