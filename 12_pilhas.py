698d7ce0c69bd8dc92bc424a1ae71a738b76eb6e
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2021-10-03 22:50:57 -0300

    ATT
    att

--- 12_pilhas.py
+++ 12_pilhas.py
@@ -1,41 +1,40 @@
-# Palíndromo: é um texto que, quando lido de trás pra frente, mantem o mesmo contéudo (Desprezando acentos e espacamento)
-
-# texto = "SOCORRAM-ME, SUBI NO ONIBUS EM MARROCOS"
-
+# Palíndromo: é um texto que, quando lido de trás para frente,
+# mantém o mesmo conteúdo (desprezando acentos e espaçamento)
+# texto = "SOCORRAM-ME, SUBI NO ÔNIBUS EM MARROCOS"
 texto = "BATATINHA QUANDO NASCE ESPALHA RAMA PELO CHÃO"
 
 pilha = []
 
-# Percurso do palindromo em ordem inversa, colocando cada letra na lista de trás pra frente
+# Percurso do palíndromo, colocando cada letra na lista
+for letra in texto:
+    pilha.append(letra) # append() sempre acrescenta por 
+    
+# pilha.insert(10, 'y')
+# pilha.insert(19, 'g')
+# pilha.insert(6, 'ç')
 
-#for x in range (len(palindromo), -1, -1, -1):
-#   lista.append (palindromo[x])
-
-#print(lista)
-
-for letra in (texto):
-    pilha.append (letra)    # append () sempre acrescenta por ULTIMO.
-
-#pilha.insert (10, 'y')
-#pilha.insert (19, 'g')
-#pilha.insert (6, 'ç')
+print(pilha)
 
 inverso = ""
 
 # Remoção de elementos em posições que não são a final
-# del pilha [11] # Remove a posição desejada.
-# del pilha [21]
-# del pilha [8]
-# del pilha [25]
+#del pilha[11]   # Remove a posição 11
+#del pilha[21]   # Remove a posição 21
+#del pilha[8]
+#del pilha[25]
 
-# Retira cada letra da lista, de trás pra frente, e coloca no inverso
+# Retira cada letra da lista, de trás para frente, e coloca no inverso
 while len(pilha) > 0:
-    inverso += pilha.pop() #  pop() retira sempre o ULTIMO elemento
+    inverso += pilha.pop()  # pop() retira sempre o ÚLTIMO elemento
 
 print(inverso)
 
 # PILHA
-# A pilha  é um tipo abstrado de dado (TAD) que permite a entrada e a saída de dados apenas na sua extremidade final.
-# Como consequencia, ela segue a regre LIFO (Last in, first out - Ultimo a entrar, primeiro a sair).
-# E tem acesso limitado aos seus elementos.
-# Pilha 
+# A pilha é um tipo abstrato de dados (TAD) que permite a entrada e a saída
+# de dados apenas na sua extremidade final. Como consequência, ela segue a
+# regra LIFO (last in, first out - último a entrar, primeiro a sair), e tem
+# acesso limitado aos seus elementos.
+
+# 2 + { 3 - [5 * (2 + 7)}]
+
+
