1d4138c0c83b0adfefb66db436bd992e7ff3d762
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2021-11-21 21:02:58 -0300

    att

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -123,45 +123,139 @@ class BinarySearchTree:
             self.pre_order_traversal(fnCallback, root.left) # 2º
             self.pre_order_traversal(fnCallback, root.right) # 3º
 
+    """
+        Método que faz o percurso pós-ordem (post-order traversal)
+        1º: visita pós-ordem a subárvore esquerda
+        2º: visita pós-ordem a subárvore direita
+        3º: visita a raiz
+        Este percurso é utilizado para sumarizações "bottom-up" (de
+        baixo para cima, das folhas em direção à raiz) e também pode
+        ser usado no processo de remoção de um nodo da árvore
+    """
+    def post_order_traversal(self, fnCallback, root = False):
 
-######################################################################
+        if root is False: root = self.__root
 
-arvore = BinarySearchTree()
+        if root is not None:
+            self.post_order_traversal(fnCallback, root.left) # 1º
+            self.post_order_traversal(fnCallback, root.right) # 2º
+            fnCallback(root.data)   # 3º
 
-arvore.insert(43)
-arvore.insert(27)
-arvore.insert(64)
-arvore.insert(36)
-arvore.insert(10)
-arvore.insert(0)
+    """
+        Método PRIVADO que busca recursivamente por um valor na árvore
+        Retorna:
+            - o nodo que contém o valor, caso este exista
+            - None, se não for encontrado
+    """
+    def __search_node(self, root, key):
 
-print(arvore.to_str())
+        # 1º caso: árvore vazia
+        if root is None: return None
 
-em_ordem = []
+        # 2º caso: o valor de key é MENOR que valor da raiz
+        # Continua a busca recursivamente pela subárvore ESQUERDA
+        if key < root.data: return self.__search_node(root.left, key)
 
-def insere_em_ordem(val):
-    em_ordem.append(val)
+        # 3º caso: o valor de key é MAIOR que o valor da raiz
+        # Continua a busca recursivamente pela subárvore DIREITA
+        if key > root.data: return self.__search_node(root.right, key)
 
-#arvore.in_order_traversal(insere_em_ordem)
-arvore.in_order_traversal(lambda val: em_ordem.append(val))
-#arvore.in_order_traversal(lambda val: print(val))
+        # 4º caso: o valor de key é IGUAL ao valor da raiz
+        # ENCONTROU O VALOR; retorna o nodo root
+        return root
 
-print('Percurso em-ordem:', em_ordem)
+    """
+        Método público que retorna se um valor existe na árvore (True)
+        ou não (False)
+    """
+    def exists(self, val):
+        node = self.__search_node(self.__root, val)
+        if node is None: return False
+        else: return True
 
-sumario = BinarySearchTree()
+    """
+        Método PRIVADO para encontrar o nodo de menor valor a partir
+        da raiz fornecida
+    """
+    def __min_node(self, root):
+        node = root
+        while node is not None and node.left is not None:
+            node = node.left
+        return node
 
-sumario.insert('2')
-sumario.insert('1')
-sumario.insert('3')
-sumario.insert('1.1')
-sumario.insert('3.1')
-sumario.insert('2.1')
-sumario.insert('2.1.1')
+    """
+        Método PRIVADO para encontrar o nodo de maior valor a partir
+        da raiz fornecida
+    """
+    def __max_node(self, root):
+        node = root
+        while node is not None and node.right is not None:
+            node = node.right
+        return node
 
-em_ordem = []
-sumario.in_order_traversal(lambda val: em_ordem.append(val))
-print('Sumário em-ordem:', em_ordem)
+    """
+        Método público para remoção de um valor da árvore
+    """
+    def remove(self, val):
+        self.__root = self.__remove_node(self.__root, val)
 
-pre_ordem = []
-sumario.pre_order_traversal(lambda val: pre_ordem.append(val))
-print('Sumário pré-ordem:', pre_ordem)
+    """
+        Método PRIVADO para remoção de um nodo da árvore
+    """
+    def __remove_node(self, root, val):
+
+        # 1º caso: árvore vazia
+        if root is None: return None
+
+        # 2º caso: o valor a ser removido é MENOR que o valor da raiz
+        # Continua procurando pelo nodo a ser removido pelo lado ESQUERDO
+        if val < root.data: 
+            root.left = self.__remove_node(root.left, val)
+            return root
+
+        # 3º caso: o valor a ser removido é MAIOR que o valor da raiz
+        # Continua procurando pelo nodo a ser removido pelo lado DIREITO
+        if val > root.data:
+            root.right = self.__remove_node(root.right, val)
+            return root
+
+        # 4º caso: o valor a ser removido é IGUAL ao valor da raiz
+        # O nodo a ser removido foi encontrado; agora é necessário
+        # determinar o grau do nodo para aplicar o algoritmo de remoção
+        # correto para cada grau
+
+        # 4.1: remoção de nodo de grau 0
+        if root.left is None and root.right is None:
+            root = None
+            return root
+
+        # 4.2: remoção de nodo de grau 1 com subárvore à esquerda
+        if root.left is not None and root.right is None:
+            root = root.left
+            return root
+
+        # 4.3: remoção do nodo de grau 1 com subárvore à direita
+        if root.left is None and root.right is not None:
+            root = root.right
+            return root
+
+        # 4.4: remoção do nodo de grau 2
+
+        # Precisamos encontrar:
+        # a) O MAIOR nodo da subárvore ESQUERDA; *ou*
+        # b) O MENOR nodo da subárvore DIREITA
+
+        # Nossa opção: usar o maior nodo da subárvore esquerda
+        new_root = self.__max_node(root.left)
+        # Ou: new_root = self.__min_node(root.right)
+
+        # Copia o valor do nodo encontrado para o nodo que está
+        # sendo "removido"
+        root.data = new_root.data
+
+        # Exclui o valor duplicado que está na subárvore esquerda
+        # (de onde veio o valor de new_root)
+        root.left = self.__remove_node(root.left, new_root.data)
+        # Ou: root.right = self.__remove_node(root.right, new_root.data)
+
+        return root
